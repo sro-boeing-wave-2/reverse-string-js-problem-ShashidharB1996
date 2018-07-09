87acc55a38b4b05dee8a6531bf81ab1de92b3a23
Shashhidhar B <shashishashidhar70@gmail.com>
2018-07-09 16:49:06 +0530

    reverse string assignment done!

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,17 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+const reverse = (str) => {
+  // Use the split() method to return a new array
+  const splitString = str.split('');
+
+  // Use the reverse() method to reverse the new created array
+  const reverseArray = splitString.reverse();
+
+  // Use the join() method to join all elements of the array into a string
+  const joinArray = reverseArray.join('');
+
+  // Return the reversed string
+  return joinArray;
+};
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,9 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
-
+const reverse = (str) => {
+  let out = ' ';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    out += str[i];
+  }
+  return out;
+};
 module.exports = reverse;

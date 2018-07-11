36930dc49332ae0c0a18ac5ae36756ec5768442c
Shashhidhar B <shashishashidhar70@gmail.com>
2018-07-11 10:15:34 +0530

    removed the extra code

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,17 +1,9 @@
 // The intent of this file is to reverse
 // the string using built in functions.
 const reverse = (str) => {
-  // Use the split() method to return a new array
-  const splitString = str.split('');
-
-  // Use the reverse() method to reverse the new created array
-  const reverseArray = splitString.reverse();
-
-  // Use the join() method to join all elements of the array into a string
-  const joinArray = reverseArray.join('');
-
-  // Return the reversed string
-  return joinArray;
+  const str1 = str.split('');
+  const str2 = str1.reverse();
+  const revStr = str2.join('');
+  return revStr;
 };
-
 module.exports = reverse;
